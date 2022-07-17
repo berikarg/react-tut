54d9217227c94e4909d4b126700236bf20f235a3
Berik Argimbayev <berikarg@gmail.com>
2022-07-17 15:53:17 +0600

    part 1 save

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="App-footer">
+            &copy; 2022 Argimbayev development. All rights reserved
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/MainContents.js
@@ -0,0 +1,14 @@
+function MainContents() {
+    return (
+        <div className="main">
+            <h1>Reasons I am excited to learn React</h1>
+            <ol>
+                <li>one</li>
+                <li>two</li>
+                <li>three</li>
+            </ol>
+        </div>
+    );
+}
+
+export default MainContents;
